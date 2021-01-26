82d95945f242314c1f668ed7fc1c964c68053061
0015814 <0015814@host.docker.internal>
2021-01-26 17:59:56 +0100

    Version Sprint1, integracion evento + llamada MS

--- src/main/java/com/atalaya/AtalayaApplication.java
+++ src/main/java/com/atalaya/AtalayaApplication.java
@@ -39,40 +39,55 @@ public class AtalayaApplication {
 		Analisis analisis = analisisrepo.findByNombre(nombre);
 		if (analisis!=null)
 		{	
-			ArrayList<String> mensaje = new ArrayList<String>();
+			//ArrayList<String> mensaje = new ArrayList<String>();
+			StringBuffer mensaje = new StringBuffer();
 			int iError = 0;
+			
+			mensaje.append("</br>Ejecutado el analisis: <b>"+ analisis.getNombre() + "</b></br>");
 			
 			for (int c = 0; c < analisis.getCriterios().size(); c++) {								
 				Criterio criAnaliza = analisis.getCriterios().get(c);					
-				Evaluador criEvalua = new Evaluador (criAnaliza.getEvaluacion(), analisis.getIndicadores());					
-				cumplido = criEvalua.evaluar();					
+				Evaluador criEvalua = new Evaluador (criAnaliza, analisis.getIndicadores(), analisis.getEventos());					
+				cumplido = criEvalua.evaluar();	
+				
 				switch (cumplido) {
 				case -1:
 				// Se han detecado errores en la evaluación
-					mensaje.add(iError, "La condición indicada en el criterio " + criAnaliza.getNombre() + " no es correcta.");
+					//mensaje.add(iError, "La condición indicada en el criterio " + criAnaliza.getNombre() + " no es correcta.");
+					mensaje.append("<br>La condición indicada en el criterio <b>" + criAnaliza.getNombre() + "</b> no es correcta.</br>");
 					iError++;
 			    	for (int e = 0; e < criEvalua.getErrores().size(); e++) {
-			    		mensaje.add(iError, criEvalua.getErrores().get(e));
+			    		//mensaje.add(iError, criEvalua.getErrores().get(e));
+			    		mensaje.append("<br><b>"+criEvalua.getErrores().get(e)+"</b></br>");
 			    		iError++;
 			    	}	
 			    	break;
 				case 0:
 				   // La evaluación ha dado como resultado que se no cumple. No deben generarse los eventos asociados al criterio.
-					mensaje.add(iError, "El criterio " + criAnaliza.getNombre() + " no cumple la condición. No se generan eventos.");
+					//mensaje.add(iError, "El criterio " + criAnaliza.getNombre() + " no cumple la condición. No se generan eventos.");
+					mensaje.append("<br>El criterio <b>" + criAnaliza.getNombre() + "</b> no cumple la condición. No se generan eventos.</br>");
 					iError++;
 					break;
 				case 1:
 				// La evaluación ha dado como resultado que se cumple. Deben generarse los eventos asociados al criterio.
-					mensaje.add(iError, "El criterio " + criAnaliza.getNombre() + " cumple la condición. Se generan los eventos siguientes:");
-					analisis.eventosCriterio(criEvalua.getEventos());
+					//mensaje.add(iError, "El criterio " + criAnaliza.getNombre() + " cumple la condición '" +criAnaliza.getEvaluacion() + "'");
+					mensaje.append("<br>El criterio <b>" + criAnaliza.getNombre() + "</b> cumple la condición <b>'" +criAnaliza.getEvaluacion() + "'</b> </br>");
+					//mensaje.add(iError, "Se han generado los siguientes eventos:");
+					mensaje.append("</br>Se han generado los siguientes eventos:</br>");
+					for (int e = 0; e < criEvalua.getEventos().size(); e++)
+					{
+						//mensaje.add(iError, criEvalua.getEventos().get(e).getNombre() + " ejecutado con el resultado: "+ criEvalua.getEventos().get(e).getResultadoEjecucion());
+						mensaje.append("</br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<b>"+criEvalua.getEventos().get(e).getEvento().getNombre() + "</b> ejecutado con el resultado: <b>"+ criEvalua.getEventos().get(e).getResultadoEjecucion() + "</b></br>");
+					}
+					
 					iError++;
 					break;
 				}
 			}
 			
+			String resultado = mensaje.toString();
 			
-			
-			return ("Ejecutado el analisis con nombre: "+ nombre+" con resultado "+cumplido);
+			return (resultado);
 		}
 		
 		else

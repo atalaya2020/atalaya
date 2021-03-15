355f25499852c67c1a4e8fbc7dd00990600aeaeb
atalaya2020 <76046500+atalaya2020@users.noreply.github.com>
2021-03-15 15:57:42 +0100

    Merge inclusion campo destino

--- src/main/java/com/modelodatos/Indicador.java
+++ src/main/java/com/modelodatos/Indicador.java
@@ -13,6 +13,7 @@ public class Indicador {
 	private String nombre;
 	private String descripcion;
 	private String fuente;
+	private String destino;
 	private String tipo;
 	private String comando;
 	private ArrayList<Parametro> parametros;
@@ -20,12 +21,13 @@ public class Indicador {
 	private ArrayList<Configuracion> configuraciones;
 	private boolean stopper;
 
-	public Indicador(String nombre, String descripcion, String fuente, String tipo, String comando,
+	public Indicador(String nombre, String descripcion, String fuente, String destino, String tipo, String comando,
 			ArrayList<Parametro> parametros, String[] resultado, ArrayList<Configuracion> configuraciones, boolean stopper) {
 		super();
 		this.nombre = nombre;
 		this.descripcion = descripcion;
 		this.fuente = fuente;
+		this.destino = destino;
 		this.tipo = tipo;
 		this.comando = comando;
 		this.parametros = parametros;
@@ -34,6 +36,17 @@ public class Indicador {
 		this.stopper = stopper;
 	}
 	
+
+	public String getDestino() {
+		return destino;
+	}
+
+
+
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}

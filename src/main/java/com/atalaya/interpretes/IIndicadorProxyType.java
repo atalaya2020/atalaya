6913b890d397bdbceff06485abeef367a4631f0c
= <=>
2021-03-22 10:28:29 +0100

    Cambios Miguel y Jorge

--- src/main/java/com/atalaya/interpretes/IIndicadorProxyType.java
+++ src/main/java/com/atalaya/interpretes/IIndicadorProxyType.java
@@ -14,6 +14,14 @@ public interface IIndicadorProxyType {
 	
 	final static String separadorFichero = "\\|";
 
+	final static String propiedad_url = "url";
+	final static String propiedad_user = "user";
+	final static String propiedad_password = "password";
+	final static String propiedad_cachePrepStmts = "cachePrepStmts";
+	final static String propiedad_prepStmtCacheSize = "prepStmtCacheSize";
+	final static String propiedad_prepStmtCacheSqlLimit = "prepStmtCacheSqlLimit";
+	final static String propiedad_maximumpoolsize = "maximumpoolsize";
+
 	//"/var/lib/sistemaFicheros/"
 	
 	public boolean ejecutar(IndicadorProxy IndicadorProxy) throws Exception;
